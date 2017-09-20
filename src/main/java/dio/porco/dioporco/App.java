1f245cd34522c2a64b549daff1b7d5225fab7587
iugy91 <eugenio.bertini.91@gmail.com>
2017-09-20 17:11:03 +0200

    primo commit del dioporco a seguito di configurazione eclipse +maven

---
+++ src/main/java/dio/porco/dioporco/App.java
@@ -0,0 +1,13 @@
+package dio.porco.dioporco;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
