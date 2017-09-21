580876f431502bebecf0fe27564afd16ea126b2e
iugy91 <eugenio.bertini.91@gmail.com>
2017-09-21 14:55:36 +0200

    inserita classe di test e recuperato sorgenti da https://www.tutorialspoint.com/junit/junit_basic_usage.htm

---
+++ src/test/java/dio/porco/dioporco/TestJunit.java
@@ -0,0 +1,13 @@
+package dio.porco.dioporco;
+
+import static org.junit.Assert.assertEquals;
+public class TestJunit {
+	
+   String message = "Hello World";	
+   MessageUtil messageUtil = new MessageUtil(message);
+
+   //@Test
+   public void testPrintMessage() {
+      assertEquals(message,messageUtil.printMessage());
+   }
+}
